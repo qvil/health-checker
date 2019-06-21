5d16528a639cb2c3a1493c08a884a5d606d54791
현태수 <qvil@op.gg>
2019-06-22 05:30:18 +0900

    List MUI로 만들고 Icon 스타일링 중

--- src/components/ServerList.jsx
+++ src/components/ServerList.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from "react";
-import axios from "axios";
 import styled from "styled-components";
-import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import CachedIcon from "@material-ui/icons/Cached";
+import WifiIcon from "@material-ui/icons/Wifi";
+import teal from "@material-ui/core/colors/teal";
+import pink from "@material-ui/core/colors/pink";
 
-const List = styled.li`
-  display: flex;
-  justify-content: space-between;
-  margin: 8px;
+const StyledWifiIcon = styled(WifiIcon)`
+  color: ${({ styledcolor }) => styledcolor};
 `;
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper
+  },
+  ok: {
+    backgroundColor: "grren"
+  },
+  status: {
+    ok: {
+      background: "green"
+    }
+  }
+}));
 
-const ServerList = ({ url }) => {
+const ServerList = ({ url, index }) => {
+  const classes = useStyles();
   const [state, setState] = useState({
     loading: false,
     error: null,
@@ -35,16 +59,48 @@ const ServerList = ({ url }) => {
       })
       .catch(error => setState({ ...state, loading: false, error }));
   };
-  const { loading, error, data } = state;
+  // const { loading, error, data } = state;
   return (
-    <List>
-      <span>{url}</span>
-      <Button onClick={checkHealth(url)} variant="contained" color="primary">
-        Check
-      </Button>
-      {loading && <span>Loading...</span>}
+    <List className={classes.root}>
+      <ListItem
+        key={index}
+        role={undefined}
+        dense
+        button
+        // onClick={handleToggle(value)}
+      >
+        <ListItemIcon>
+          <Checkbox
+            edge="start"
+            // checked={checked.indexOf(value) !== -1}
+            tabIndex={-1}
+            disableRipple
+            inputProps={{ "aria-labelledby": url }}
+          />
+        </ListItemIcon>
+        <ListItemText id={`checkbox-list-label-${url}`} primary={url} />
+        <ListItemSecondaryAction>
+          <IconButton
+            edge="start"
+            aria-label="Status"
+            onClick={checkHealth(url)}
+          >
+            {/* rotate animation 넣기 */}
+            <CachedIcon />
+          </IconButton>
+          <IconButton
+            edge="end"
+            aria-label="HealthCheck"
+            onClick={checkHealth(url)}
+          >
+            {/* setTimeout 걸어서 시간 지날 때 마다 와이파이 칸 떨어지게 */}
+            <StyledWifiIcon styledcolor={index === 1 ? teal[500] : pink[500]} />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      {/* {loading && <span>Loading...</span>}
       {!loading && error && <span>Error</span>}
-      {!loading && !error && data && <span>{data}</span>}
+      {!loading && !error && data && <span>{data}</span>} */}
     </List>
   );
 };
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,26 +1,21 @@
 import React from "react";
-// import useAxios from "../hooks/useAxios";
 import ServerList from "../components/ServerList";
-import Icon from "@material-ui/core/Icon";
 import { Container } from "../components";
 import withCardLayout from "../components/withCardLayout";
 
 const lists = [
-  { url: "//www.naver.com" },
-  { url: "//www.op.gg" },
-  { url: "//www.daum.net" }
+  { url: "https://www.naver.com" },
+  { url: "https://www.op.gg" },
+  { url: "https://www.daum.net" }
 ];
 
 const List = () => {
   return (
     <Container>
       <h1>List</h1>
-      <ol>
-        {lists.map(({ url }, index) => (
-          <ServerList key={index} url={url} />
-        ))}
-      </ol>
-      <Icon>star</Icon>
+      {lists.map(({ url }, index) => (
+        <ServerList key={index} index={index} url={url} />
+      ))}
     </Container>
   );
 };
