13c4e1ebca0b14c74b9c1a32f2ab9353049824d0
Taesu Hyeon <qvil1127@gmail.com>
2019-07-05 03:16:12 +0900

    Add .env.sample, useAxios from NPM
    - But I think create custom hook for API

--- src/components/ServerList.jsx
+++ src/components/ServerList.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { makeStyles } from "@material-ui/core/styles";
-import axios from "axios";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -14,7 +13,8 @@ import WifiIcon from "@material-ui/icons/Wifi";
 import teal from "@material-ui/core/colors/teal";
 import pink from "@material-ui/core/colors/pink";
 import { animation } from "../styles/mixin";
-import useAxios from "@react-qooks/use-axios";
+// import useAxios from "@react-qooks/use-axios";
+import useAxios from "@react-daily-hooks/use-axios";
 
 const StyledWifiIcon = styled(WifiIcon)`
   color: ${({ styledcolor }) => styledcolor};
@@ -49,35 +49,29 @@ const StyledCachedIcon = styled(CachedIcon)`
 
 const ServerList = ({ url, index }) => {
   const classes = useStyles();
-  const [state, setState] = useState({
-    loading: false,
-    error: null,
-    data: null,
-    trigger: false
-  });
-  const render = useAxios(
-    "http://ddragon.leagueoflegends.com/api/versions.json",
-    state.trigger
+  const [fetchTrigger, setFetchTrigger] = useState(false);
+  const { loading, error, data } = useAxios(
+    { url: `${process.env.REACT_APP_API_URL}/email@qvil.dev` },
+    fetchTrigger
   );
 
+  console.log("TCL: ServerList -> data", data);
   const checkHealth = () => () => {
     // Hooks
     // fetch();
-
-    setState({ ...state, loading: true });
     // API
     // axios(url)
-    axios("http://ddragon.leagueoflegends.com/api/versions.json")
-      .then(res => {
-        console.log(res);
-        if (res.status === 200) {
-          const data = res.data;
-          setState({ ...state, loading: false, data });
-        } else {
-          throw new Error();
-        }
-      })
-      .catch(error => setState({ ...state, loading: false, error }));
+    // axios("http://ddragon.leagueoflegends.com/api/versions.json")
+    //   .then(res => {
+    //     console.log(res);
+    //     if (res.status === 200) {
+    //       const data = res.data;
+    //       setState({ ...state, data });
+    //     } else {
+    //       throw new Error();
+    //     }
+    //   })
+    //   .catch(error => setState({ ...state, error }));
   };
   // const { loading, error, data } = state;
   return (
@@ -87,7 +81,7 @@ const ServerList = ({ url, index }) => {
         role={undefined}
         dense
         button
-        onClick={() => setState({ trigger: true })}
+        onClick={() => setFetchTrigger(true)}
       >
         <ListItemIcon>
           <Checkbox
@@ -108,19 +102,17 @@ const ServerList = ({ url, index }) => {
             <StyledCachedIcon loading />
           </StyledIconButton>
           {/* 이것보다 loading, error, data state관리가 더 나은듯 */}
-          {render({
-            loading: () => (
-              <StyledIconButton
-                edge="start"
-                aria-label="Status"
-                onClick={checkHealth(url)}
-              >
-                <CachedIcon />
-              </StyledIconButton>
-            ),
-            error: error => <div>{error.toString()}</div>,
-            data: data => <div>{JSON.stringify(data)}</div>
-          })}
+          {loading && (
+            <StyledIconButton
+              edge="start"
+              aria-label="Status"
+              onClick={checkHealth(url)}
+            >
+              <CachedIcon />
+            </StyledIconButton>
+          )}
+          {error && <div>{error.toString()}</div>}
+          {!loading && !error && data && <div>{JSON.stringify(data)}</div>})}
           {/* setTimeout 걸어서 시간 지날 때 마다 와이파이 칸 떨어지게 */}
           <IconButton
             edge="end"
